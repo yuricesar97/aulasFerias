5e28a89cb9c94f4c6fcbf8e45d2d9b119b23dbfe
Yuri Cesar <yuricesar97@gmail.com>
2019-02-04 15:31:50 -0200

    Projeto criado

---
+++ src/main/java/com/yuri/aulas/EstudosferiasApplication.java
@@ -0,0 +1,14 @@
+package com.yuri.aulas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EstudosferiasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EstudosferiasApplication.class, args);
+	}
+
+}
+

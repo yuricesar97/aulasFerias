c64e4bbf54db2154236759f339f787e4e7cd7378
Yuri Cesar <yuricesar97@gmail.com>
2019-02-05 12:23:09 -0200

    Testado primeria classe de dominio categorio

--- src/main/java/com/yuri/aulas/resource/CategoriasResource.java
+++ src/main/java/com/yuri/aulas/resource/CategoriasResource.java
@@ -1,17 +1,34 @@
 package com.yuri.aulas.resource;
 
 
+
+
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.yuri.aulas.domain.Categoria;
 
 @RestController
 @RequestMapping(value = "/categorias")
 public class CategoriasResource {
  
 	@RequestMapping(method=RequestMethod.GET)
-	public String lista() {
-		return "Rest esta funcionando";
+	public List<Categoria> lista() {
+		
+		Categoria cat1 = new Categoria(1, "informatica");
+		Categoria cat2 = new Categoria(2, "Escritorio");
+		
+		List<Categoria>lista = new ArrayList<>();
+		lista.add(cat1);
+		lista.add(cat2);
+		
+		
+		
+		return lista;
 		
 	}
 	

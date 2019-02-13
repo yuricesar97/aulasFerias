b987f93b3fe94c5dd91da22bd42db36875563895
Yuri Cesar <yuricesar97@gmail.com>
2019-02-13 11:52:09 -0200

    Produto e associacao muito para muito

--- src/main/java/com/yuri/aulas/EstudosferiasApplication.java
+++ src/main/java/com/yuri/aulas/EstudosferiasApplication.java
@@ -1,6 +1,6 @@
 package com.yuri.aulas;
 
-import java.util.ArrayList;
+
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,16 +9,21 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 import com.yuri.aulas.domain.Categoria;
+import com.yuri.aulas.domain.Produto;
 import com.yuri.aulas.repositories.CategoriasRepositoty;
-import com.yuri.aulas.service.CategoriaService;
+import com.yuri.aulas.repositories.ProdutoRepositoty;
+
+
 
 @SpringBootApplication
 public class EstudosferiasApplication implements CommandLineRunner {
 
 	@Autowired
 	private CategoriasRepositoty categoriaRepository;
+	@Autowired
+	private ProdutoRepositoty produtoRepository;
 	
-	public static void main(String[] args) {
+	public static void main(String[] args)  {
 		SpringApplication.run(EstudosferiasApplication.class, args);
 	}
 
@@ -27,7 +32,20 @@ public class EstudosferiasApplication implements CommandLineRunner {
 		Categoria cat1 = new Categoria(null, "Informatica");
 		Categoria cat2 = new Categoria(null, "Escritorio");
 		
+		Produto produto1 = new Produto(null, "Computador", 2000.00);
+		Produto produto2 = new Produto(null, "Impressora", 800.00);
+		Produto produto3 = new Produto(null, "Mouse", 80.00);
+		
+		
+		cat1.getProdutos().addAll(Arrays.asList(produto1,produto2,produto3));
+		cat2.getProdutos().addAll(Arrays.asList(produto2));
+		
+		produto1.getCategorias().addAll(Arrays.asList(cat1));
+		produto2.getCategorias().addAll(Arrays.asList(cat1,cat2));
+		produto3.getCategorias().addAll(Arrays.asList(cat1));
+		
 		categoriaRepository.saveAll(Arrays.asList(cat1,cat2));
+		produtoRepository.saveAll(Arrays.asList(produto1,produto2,produto3));
 		
 	}
 
